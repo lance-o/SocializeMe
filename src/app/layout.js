f11c9b9825f92bab3c77c957c1c1024c13d9f51d
subhadra30 <k.subhadra@gmail.com>
2024-09-13 10:50:06 +0100

    p tag added in layoutjs

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,6 +30,7 @@ export default function RootLayout({ children }) {
           <Header />
 
           <UploadMedia />
+          <p>This is a test line</p>
           {children}
         </ClerkProvider>
       </body>
