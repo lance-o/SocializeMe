f20be1eefb63d13b2dcacf6c1ac38c09a05ab916
khashayarRohani <khashayarrhi@gmail.com>
2024-09-13 11:26:57 +0100

    test is done

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,15 +23,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <p>Lawrences change</p>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ClerkProvider>
           <Header />
 
-
-        
           <p>This is a test line</p>
 
           {children}
