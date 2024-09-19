cf4fc134520a15a05ceab958fd29ecd7317ec2e7
Lawrence Cilia <cilialawrence@yahoo.co.uk>
2024-09-19 15:39:45 +0100

    speed up query so it's done

--- src/app/actions/searchByCat.js
+++ src/app/actions/searchByCat.js
@@ -5,12 +5,8 @@ import { db } from "@/lib/db";
 export async function searchByCat(cat) {
   if(cat === "*")
     return null;
-  const catRes = await db.query(`SELECT * FROM categories WHERE id =$1`, [
-    cat,
-  ]);
-  const categoryId = catRes.rows[0].id;
   const postsRes = await db.query("SELECT * FROM posts WHERE category_id=$1 ORDER BY posts.creation_date DESC", [
-    categoryId,
+    cat,
   ]);
 
   const posts = postsRes.rows;
