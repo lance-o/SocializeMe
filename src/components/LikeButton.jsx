8ad5cfc36c60c04061f91773c63730e0fc7a24a8
khashayarRohani <khashayarrhi@gmail.com>
2024-09-19 12:39:45 +0100

    adding some css

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -6,17 +6,22 @@ export function LikeButton({ like, existingLike, numLikes }) {
   const { pending, data, method, action } = useFormStatus();
   return (
     <>
-      {existingLike 
-      ? <button formAction={like} className="border relative text-left rounded px-2 bg-green-300 hover:bg-red-400 before:content-['Liked'] hover:before:content-['Unlike']">
-        <p className="absolute right-2 top-0">{numLikes}</p>
-      </button>
-
-      : <button formAction={like} className="border relative text-left rounded px-2 hover:bg-green-200">
-        Like
-        <p className="absolute right-2 top-0">{numLikes}</p>
-        </button>}
-
-      
+      {existingLike ? (
+        <button
+          formAction={like}
+          className="border relative text-left rounded px-2 bg-green-300 hover:bg-red-400 before:content-['Liked'] hover:before:content-['Unlike']"
+        >
+          <p className="absolute right-2 top-0">{numLikes}</p>
+        </button>
+      ) : (
+        <button
+          formAction={like}
+          className="border relative text-left rounded px-2 hover:bg-green-200"
+        >
+          Like
+          <p className="absolute right-2 top-0">{numLikes}</p>
+        </button>
+      )}
     </>
   );
 }
